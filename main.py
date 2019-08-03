41f78295e0ce088bcf4a6362364e018a8a6b7e2e
subash774 <subash.poudyal8@gmail.com>
2019-08-03 04:02:05 +0100

    Login working, streaming and recording live

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, Response, render_template, request
+from flask import Flask, Response, render_template, request, session
 import pyrebase
 import firebase
 from camera import VideoCamera
+import os
 
 app = Flask(__name__, template_folder="templates")
+app.secret_key = os.urandom(24)
 
 config = firebase.get_credentials()
 firebase = pyrebase.initialize_app(config)
@@ -12,21 +14,24 @@ auth = firebase.auth()
 
 def gen(feed):
     while True:
-        frame = feed.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        feed.record()
+        try:
+            frame = feed.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+            feed.record()
+        except Exception as e:
+            print(str(e))
 
 
 @app.route('/video_feed')
 def video_feed():
-    global user
     try:
-        if user:
-            return Response(gen(VideoCamera()),
-                            mimetype='multipart/x-mixed-replace; boundary=frame')
-    except Exception as e:
-        return 404
+        print(session['usr'])
+        return Response(gen(VideoCamera()),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+    except KeyError:
+        print("Wrong id")
+        return render_template('login.html', us="Unauthorised")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -36,13 +41,11 @@ def index():
         email = request.form['name']
         password = request.form['pass']
         try:
-            global user
             user = auth.sign_in_with_email_and_password(email, password)
-            if user:
-                return render_template('index.html')
-            else:
-                return render_template('login.html', us=unsuccessful)
-        except:
+            session['usr'] = user.get('idToken')
+            return render_template('index.html')
+        except Exception as e:
+            print(str(e))
             return render_template('login.html', us=unsuccessful)
 
     return render_template('login.html')

f419b02177f976e414975c28558939c6d3d216b0
subash774 <subash.poudyal8@gmail.com>
2019-08-12 19:29:20 +0100

    record every hour

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pyrebase
 import firebase
 from camera import VideoCamera
 import os
+from threading import Thread
+import datetime
 
 app = Flask(__name__, template_folder="templates")
 app.secret_key = os.urandom(24)
@@ -10,6 +12,25 @@ app.secret_key = os.urandom(24)
 config = firebase.get_credentials()
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+video = VideoCamera()
+threads = []
+
+current_hour = 100
+kill = False
+
+
+def record():
+    global video
+    while True:
+        try:
+            if not kill:
+                video.record()
+            else:
+                video.save()
+                video = VideoCamera()
+                break
+        except Exception as e:
+            print(str(e))
 
 
 def gen(feed):
@@ -18,7 +39,6 @@ def gen(feed):
             frame = feed.get_frame()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-            feed.record()
         except Exception as e:
             print(str(e))
 
@@ -27,7 +47,7 @@ def gen(feed):
 def video_feed():
     try:
         print(session['usr'])
-        return Response(gen(VideoCamera()),
+        return Response(gen(video),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
     except KeyError:
         print("Wrong id")
@@ -36,6 +56,24 @@ def video_feed():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global current_hour, threads, kill
+    print(current_hour)
+    now = datetime.datetime.now().time().hour
+    if len(threads) == 0:
+        kill = False
+        t1 = Thread(target=record)
+        threads.append(t1)
+        t1.start()
+        current_hour = now
+        print("Starting ...")
+
+    else:
+        if current_hour != now:
+            print("Not the same \n changing threads")
+            kill = True
+            del threads[:]
+            current_hour = now
+
     unsuccessful = 'Please check your credentials'
     if request.method == 'POST':
         email = request.form['name']
@@ -51,5 +89,5 @@ def index():
     return render_template('login.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=2000, debug=True, use_reloader=False)
